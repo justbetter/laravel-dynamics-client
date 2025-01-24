c49ed9ba335e38a17776dcf89d0e085caf08e950
VincentBean <VincentBean@users.noreply.github.com>
2025-01-24 13:53:46 +0000

    Fix styling changes

--- src/Exceptions/NotFoundException.php
+++ src/Exceptions/NotFoundException.php
@@ -5,8 +5,8 @@ namespace JustBetter\DynamicsClient\Exceptions;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-class NotFoundException extends DynamicsException {
-
+class NotFoundException extends DynamicsException
+{
     public function render(Request $request): Response
     {
         return response(null, 404);
